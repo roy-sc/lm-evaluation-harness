dc8dafc4e1968e6b3d69411a4e1014a5252c9bcf
Ribin Chalumattu <cribin@inf.ethz.ch>
2023-08-24 16:20:37 +0200

    Updated seahorse classification task and prompt templates

--- lm_eval/tasks/seahorse_classification.py
+++ lm_eval/tasks/seahorse_classification.py
@@ -1,15 +1,26 @@
 from functools import partial
 
+import evaluate
 import numpy as np
 
 from lm_eval.base import Task, rf
 from lm_eval.metrics import complex_metric_agg
 
+roc_auc_metric = evaluate.load("roc_auc")
+
+
+def _roc_auc_agg(items):
+    predictions, references = zip(*items)
+    result = roc_auc_metric.compute(
+        prediction_scores=predictions, references=references
+    )['roc_auc']
+    return result
+
 
 class SeahorseClassificationTask(Task):
     VERSION = 0
     # dataset as denoted in HuggingFace `datasets`.
-    DATASET_PATH = "mtc/seahorse_dataset_with_articles"
+    DATASET_PATH = "mtc/german_seahorse_dataset_with_articles"
     # `DATASET_PATH`. If there aren't specific subsets you need, leave this as `None`.
     DATASET_NAME = None
 
@@ -25,7 +36,7 @@ class SeahorseClassificationTask(Task):
     default_prompt_template = """### System:
 You are StableBeluga, an AI that follows instructions extremely well. Help as much as you can.
 
-### User: Given an article and its summary in {lang}, determine if all information in the summary is sourced from the article. Return True if it is, and False if not. 
+### User: Given an article and its corresponding summary in {lang}, reply with True if all information in the summary originates from the article and reply False otherwise. 
 Article: {article}
 Summary: {summary}
 
@@ -54,7 +65,13 @@ Summary: {summary}
 
     def test_docs(self):
         if self.has_test_docs():
-            return self.dataset["test"]
+            test_set = self.dataset["test"]
+            hq_test_set_german = test_set.filter(lambda example: example['question1'] == 'Yes')
+            print(f"Num of samples: {hq_test_set_german.num_rows}")
+            test = hq_test_set_german.filter(lambda example: example["question4"] == "Yes")
+            print(
+                f"Num of positive samples: {test.num_rows}. Num of negative samples: {hq_test_set_german.num_rows - test.num_rows}")
+            return hq_test_set_german
 
     def doc_to_text(self, doc):
         if not self.prompt_template:
@@ -85,12 +102,17 @@ Summary: {summary}
 
     def process_results(self, doc, results):
         prediction = np.argmax(results)
+        # sklearn documentation: roc prediction probability corresponds to the probability of the class with the
+        # greater label(=1)
+        results_probabilities = np.exp(results)
+        true_prediction_probability = results_probabilities[1]/np.sum(results_probabilities)
         truth = self.convert_label(self.doc_to_target(doc))
         print(f"Results: {results}, Prediction {prediction}, Truth: {truth}")
         return {"bacc": (prediction, truth),
                 "f1": (prediction, truth),
                 "precision": (prediction, truth),
-                "recall": (prediction, truth)}
+                "recall": (prediction, truth),
+                "roc_auc": (true_prediction_probability, truth)}
 
     def aggregation(self):
         return {
@@ -105,6 +127,9 @@ Summary: {summary}
             ),
             "recall": partial(
                 complex_metric_agg, "recall"
+            ),
+            "roc_auc": partial(
+                _roc_auc_agg
             )
         }
 
@@ -112,4 +137,5 @@ Summary: {summary}
         return {"bacc": True,
                 "f1": True,
                 "precision": True,
-                "recall": True}
+                "recall": True,
+                "roc_auc": True}
